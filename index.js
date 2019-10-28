474438ce051d1530f9e5f325e0c9a1c2e665cca0
Kanav Gupta <kanav0610@gmail.com>
2019-10-28 15:19:32 +0530

    files: push initial version

---
+++ index.js
@@ -0,0 +1,45 @@
+var express =       require("express");
+var fs      =       require("fs");
+var request =       require("request");
+
+config = {
+	slack: {
+		client_id: process.env.SLACK_CLIENT_ID,
+		client_secret: process.env.SLACK_CLIENT_SECRET
+	}
+}
+
+/* MIDDLEWARES */
+var app = express()
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static('public'))
+app.set('view engine', 'ejs');
+
+function code2token(code, cb) {
+	form = {client_id: config.slack.client_id, client_secret: config.slack.client_secret, code}
+	request.post({
+		url: 'https://slack.com/api/oauth.access',
+		form: form
+	}, function(err, res, body) {
+		console.log(body)
+		cb(JSON.parse(body).incoming_webhook.url);
+	})
+}
+
+app.get('/slack', (req, res) => {
+	code = req.query.code
+	if (code == undefined) {
+		res.redirect("/");
+		return;
+	}
+	code2token(code, function(token) {
+		res.render('redirect', {token})
+	})
+})
+
+app.get('/github/webhook', (req, res) => {
+	res.end();
+})
+
+app.listen(process.env.PORT || 8089)
